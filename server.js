86548afdae868c2176d5a1002ae9ec9e347f748e
Tomomi Shiba <follwthedot@gmail.com>
2022-09-15 23:19:30 +0900

    fix

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const database = {
   ]
 }
 
-app.get('/', (req, res)=> {res.send('it is working!');})
+app.get('/', (req, res)=> {res.send('it is working!')})
 app.post('/signin', (req, res) => { signin.handleSignin(req,res,db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req,res,db, bcrypt)})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req,res,db)})
