ddf5776e69c4487b7122dea17ac1ed1b547012ce
Tomomi Shiba <follwthedot@gmail.com>
2022-09-16 00:10:33 +0900

    db fix

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: '',
-    // port: 3000,
-    password: '',
-    database: 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 
